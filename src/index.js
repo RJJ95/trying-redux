4a357d8bcbd8f8089697576fc237450abed1a09a
RJJ95 <ricardo.de.vries@hotmail.com>
2020-09-17 16:48:25 +0200

    first things for reducer

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import reducer from "./store/reducer";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
