b687c5a14c5719935642d5940d109c1297bf8c89
RJJ95 <ricardo.de.vries@hotmail.com>
2020-09-17 17:17:12 +0200

    started on to do list mapping of array

--- src/pages/list-page/list-page-view.js
+++ src/pages/list-page/list-page-view.js
@@ -1,18 +1,37 @@
 import React from "react";
 import styles from "./list-page.module.css";
 
+const list = [
+  {
+    title: "Hello darkness, my old friend",
+    executed: false,
+  },
+  {
+    title: "I've come to talk to you again",
+    executed: true,
+  },
+  {
+    title: "Because a vision softly creeping",
+    executed: false,
+  },
+];
+
 const ListPage = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
-        <div className={styles.listBody}>
-          {list.map((item) => (
-            <div className={styles.itemRow}>
-              <div className={styles.itemTitle}></div>
-              <div className={styles.buttonGroup}></div>
+        <h1>Your to do list :)</h1>
+      </div>
+      <div className={styles.listBody}>
+        {list.map((item) => (
+          <div className={styles.itemRow}>
+            <div className={styles.itemTitle}>{item.title}</div>
+            <div className={styles.buttonGroup}>
+              <button className={styles.checkItem}>Mark as done</button>
+              <button className={styles.removeItem}>Remove this item</button>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
